e7b196e5f1740c502dd41cc87b166c61565b017f
Latif Benzzine <72002836+lbenzzineaws-spring@users.noreply.github.com>
2022-10-29 11:37:41 -0500

    changed few things

--- myUntrackedFile.js
+++ myUntrackedFile.js
@@ -3,5 +3,4 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Welcome class!');
-    //console.log("Another change");
 }).listen(8080);
